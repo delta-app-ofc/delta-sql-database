1aaa88a7bf61a051e3db4c3c9b24a3f48d88e157
marianamarrao <marimarrao0115@gmail.com>
2026-06-30 21:16:51 -0300

    refactor: alterar script de permissionamento de acordo com as mudanças na modelagem

--- script-roles.sql
+++ script-roles.sql
@@ -1,9 +1,9 @@
 -- 1. SYSTEM ROLES
-    CREATE ROLE sys_data_engineer         NOINHERIT;
-    CREATE ROLE sys_backend_developer     NOINHERIT;
-    CREATE ROLE sys_bi_analyst            NOINHERIT;
-    CREATE ROLE sys_devops                NOINHERIT;
-    CREATE ROLE sys_first_year            NOINHERIT;
+    CREATE ROLE sys_data_engineer;
+    CREATE ROLE sys_backend_developer;
+    CREATE ROLE sys_bi_analyst;
+    CREATE ROLE sys_devops;
+    CREATE ROLE sys_first_year;
 
 -- 2. USERS
     CREATE USER samuel         WITH PASSWORD 'CHANGE_PASSWORD_SAMUEL';
@@ -18,28 +18,28 @@
     GRANT sys_devops                TO samuel;
     GRANT sys_data_engineer         TO samuel;
     GRANT sys_bi_analyst            TO samuel;
-
     GRANT sys_data_engineer         TO mariana;
     GRANT sys_backend_developer     TO mariana;
-
     GRANT sys_backend_developer     TO davi;
     GRANT sys_backend_developer     TO joao;
     GRANT sys_bi_analyst            TO ana;
     GRANT sys_backend_developer     TO rahquel;
-
     GRANT sys_first_year            TO first_year;
 
--- 4. BACKEND - FULL CRUD
+-- 4. BACKEND
     GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE
-          tbl_user,
-          tbl_installation,
-          tbl_user_installation,
-          tbl_device,
-          tbl_consumption_goal,
-          tbl_alert,
-          tbl_habit,
-          tbl_user_habit,
-          tbl_water_tariff
+          tb_region,
+          tb_day_of_week,
+          tb_habit,
+          tb_address,
+          tb_user,
+          tb_property,
+          tb_user_property,
+          tb_device,
+          tb_region_rate,
+          tb_user_habit,
+          tb_user_habit_day,
+          tb_last_water_bill
     TO sys_backend_developer;
 
 -- 5. DATA ENGINEER
@@ -57,19 +57,15 @@
 -- 8. FIRST YEAR
     GRANT USAGE  ON SCHEMA public TO sys_first_year;
     GRANT SELECT ON ALL TABLES   IN SCHEMA public TO sys_first_year;
-
-    REVOKE INSERT, UPDATE, DELETE ON ALL TABLES    FROM sys_first_year;
-    REVOKE EXECUTE               ON ALL FUNCTIONS  FROM sys_first_year;
+    REVOKE INSERT, UPDATE, DELETE ON ALL TABLES    IN SCHEMA public FROM sys_first_year;
+    REVOKE EXECUTE               ON ALL FUNCTIONS  IN SCHEMA public FROM sys_first_year;
 
 -- 9. DEFAULT PRIVILEGES
     ALTER DEFAULT PRIVILEGES IN SCHEMA public
         GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO sys_backend_developer;
-
     ALTER DEFAULT PRIVILEGES IN SCHEMA public
         GRANT ALL ON TABLES TO sys_data_engineer;
-
     ALTER DEFAULT PRIVILEGES IN SCHEMA public
         GRANT SELECT ON TABLES TO sys_bi_analyst;
-
     ALTER DEFAULT PRIVILEGES IN SCHEMA public
         GRANT SELECT ON TABLES TO sys_first_year;

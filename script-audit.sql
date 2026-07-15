b80cfc8b29f639e6e02575aea1deaf1a4115cec4
marianamarrao <marimarrao0115@gmail.com>
2026-07-15 19:38:20 -0300

    refactor: adicionar fk que referencia a própri tabela em tb_log_user_habit_day

--- script-audit.sql
+++ script-audit.sql
@@ -2222,6 +2222,10 @@ CREATE TABLE tb_log_user_habit_day (
     , previous_log_id       INTEGER
     , log_description       TEXT
 
+    , CONSTRAINT fk_tb_log_user_habit_day_previous_log
+        FOREIGN KEY (previous_log_id)
+        REFERENCES tb_log_user_habit_day (id)
+
 );
 
 CREATE OR REPLACE FUNCTION fn_log_user_habit_day()
